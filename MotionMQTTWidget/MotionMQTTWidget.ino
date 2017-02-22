2927cbffa8a249b4367d29eb6ccbb56713557b8f
skelstar <skelstar@gmail.com>
2017-02-23 06:05:59 +1300

    Got it working! Sends dummy data to Ubidots.com

--- MotionMQTTWidget/MotionMQTTWidget.ino
+++ MotionMQTTWidget/MotionMQTTWidget.ino
@@ -1,5 +1,9 @@
+//#include <WiFi.h>
 #include <myWifiHelper.h>
+//#include "wificonfig.h"
 #include <myPushButton.h>
+//#include <UbidotsMicroESP8266.h>
+#include <ESP8266HTTPClient.h>
 
 char versionText[] = "MotionMQTTWidget v0.9";
 
@@ -7,12 +11,23 @@ char versionText[] = "MotionMQTTWidget v0.9";
 
 #define 	TOPIC_LIAMROOMDOOR_PIR	"/dev/liam-room-door-pir"
 
+#define     UBIDOT_DEVICE           "liam-room-door-sensor"
+#define     UBIDOT_VARIABLE         "motion"
+#define     UBIDOT_LIAMROOMDOOR_ID  "58abde8c76254260e42f1632"
+#define     UBIDOT_TOKEN            "GioE85MkDexLpsbE1Tt37F3TY2p3Fa6XM4bKvftz9OfC87MrH5bQGceJrVgF"
+
 #define 	PIR_PIN    		D0
 #define		PULL_UP     	true
 
 /*---------------------------------------------------------------------*/
 
 MyWifiHelper wifiHelper(WIFI_HOSTNAME);
+
+int status = WL_IDLE_STATUS;
+
+/*---------------------------------------------------------------------*/
+
+//Ubidots ubiclient("GioE85MkDexLpsbE1Tt37F3TY2p3Fa6XM4bKvftz9OfC87MrH5bQGceJrVgF");
 
 /*---------------------------------------------------------------------*/
 
@@ -28,6 +43,8 @@ void listener_PIR(int eventCode, int eventParams) {
 
             wifiHelper.mqttPublish(TOPIC_LIAMROOMDOOR_PIR, "1");
 
+            sendVariableToUbidots();
+
 			break;
 
         case pir.EV_RELEASED:
@@ -38,10 +55,12 @@ void listener_PIR(int eventCode, int eventParams) {
 /*---------------------------------------------------------------------*/
 
 void setup() {
+
     Serial.begin(9600);
     Serial.println("Booting");
 
     wifiHelper.setupWifi();
+
     wifiHelper.setupOTA(WIFI_HOSTNAME);
 
     wifiHelper.setupMqtt();
@@ -58,4 +77,8 @@ void loop() {
     delay(10);
 }
 
+void sendVariableToUbidots() {
+    String body = "{ \"value\": 1.2, \"context\":{ \"message\":\"test2\" } }";
+    wifiHelper.sendValueToUbidots(UBIDOT_DEVICE, UBIDOT_LIAMROOMDOOR_ID, UBIDOT_TOKEN, body);
+}
 /*---------------------------------------------------------------------*/
