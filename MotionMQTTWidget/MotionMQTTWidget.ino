a0e42a64ce77e517e8f5eb1119fe0a07dc045025
skelstar <skelstar@gmail.com>
2017-06-10 06:05:19 +1200

    Got LiamsRoom motion working again. Flashing LED.

--- MotionMQTTWidget/MotionMQTTWidget.ino
+++ MotionMQTTWidget/MotionMQTTWidget.ino
@@ -3,71 +3,33 @@
 #include <ArduinoJson.h>            // https://github.com/bblanchon/ArduinoJson
 #include <TaskScheduler.h>
 #include <TimeLib.h>
-#include <myUbidotVariable.h>
 #include <Adafruit_NeoPixel.h>
-#include <myLedManager.h>
 
 
-char versionText[] = "MotionMQTTWidget v2.0";
+char versionText[] = "MotionMQTTWidget v3.0";
+
+// #define     WIFI_HOSTNAME   	"/liamhouse/motion"
+
+// #define 	TOPIC_TIMESTAMP     "/dev/timestamp"
+// #define 	TOPIC_COMMAND		"/liamhouse/motion/command"
+// #define     TOPIC_ONLINE        "/liamhouse/motion/online"
+// #define     TOPIC_EVENT         "/liamhouse/motion/event"
+
+#define     WIFI_HOSTNAME   	"/liamsroom/motion"
 
 #define 	TOPIC_TIMESTAMP     "/dev/timestamp"
+#define 	TOPIC_COMMAND		"/liamsroom/motion/command"
+#define     TOPIC_ONLINE        "/liamsroom/motion/online"
+#define     TOPIC_EVENT         "/liamsroom/motion/event"
 
-enum SensorNameType {
-	PIR_LID,
-	PIR_BOXEND,
-	PIR_BOXEND2,
-	PIR_ROOM
-};
+#define     PIR_PIN         D0	// WEMOS D0, ESP 2
+#define 	PIXEL_PIN		D1	// WEMOS D1
 
-// struct PirUnitType {
-// 	wifiHostName;
-// 	topicPublish;
-// 	topicOnline;
-// 	sensorName; 
-// 	pirPin;      
-// };
-
-// ====================================================
-// #define     WIFI_HOSTNAME   "PIR_LIAMROOM_LID"
-// #define     TOPIC_MOTION   	"/dev/liamroom-lid-motion"
-// #define     TOPIC_ONLINE    "/dev/liamroom-lid-online"
-// #define     SENSOR_NAME    "LID"
-// #define     PIR_PIN         D0    // ESP8266-01: 2, WEMOS D0
-// SensorNameType sensor = PIR_LID;
-
-#define     WIFI_HOSTNAME   "liamroom-room-motion"
-#define     TOPIC_MOTION   	"/device/liamroom/motion"
-#define     TOPIC_ONLINE    "/device/liamroom/motion/online"
-#define     TOPIC_COMMAND   "/device/liamroom/motion/command"
-#define     SENSOR_NAME    	"BOXEND"
-#define     PIR_PIN         D0    // ESP8266-01: 2, WEMOS D0
-#define 	PIXEL_PIN		D1
-SensorNameType sensor = PIR_BOXEND;
-
-bool send_enabled = false;
-
-// ====================================================
-
-#define     MQTT_FEED_TIMESTAMP "/dev/timestamp"
-
-#define     SECONDS             1000
-#define     PIR_OFFLINE_PERIOD  10 * SECONDS  // ten seconds
-
-#define     BLUE_LED         1 // pin 1 on -01
-
-#define     PIR_TRIGGER_LED 14
-
-bool        pirsEnabled = true;
-
-/*---------------------------------------------------------------------*/
+//--------------------------------------------------------------------------------
 
 MyWifiHelper wifiHelper(WIFI_HOSTNAME);
 
-int status = WL_IDLE_STATUS;
-
-myLedManager ledManager(1);
-
-/*---------------------------------------------------------------------*/
+//--------------------------------------------------------------------------------
 
 #define     PULL_UP                 true
 #define     NO_PULL_UP              false
@@ -75,109 +37,74 @@ myLedManager ledManager(1);
 #define     NORMAL_HIGH             HIGH
 
 void pir_callback(int eventCode, int eventParams);
-myPushButton pir(PIR_PIN, NO_PULL_UP, 100000, NORMAL_LOW, pir_callback);
+myPushButton pir(PIR_PIN, PULL_UP, 100000, NORMAL_LOW, pir_callback);
+void pir_callback(int eventCode, int eventParams) {
 
-/*---------------------------------------------------------------------*/
-#define NUM_PIXELS	1
+	switch (eventParams) {
+		case pir.EV_BUTTON_PRESSED:
+			wifiHelper.mqttPublish(TOPIC_EVENT, "Motion");
+			break;
+	}
+}
+
+//--------------------------------------------------------------------------------
+#define 	NUM_PIXELS		1
 
 Adafruit_NeoPixel pixel = Adafruit_NeoPixel(NUM_PIXELS, PIXEL_PIN, NEO_GRB + NEO_KHZ800);
 
 uint32_t COLOUR_OFF = pixel.Color(0, 0, 0);
 uint32_t COLOUR_MOTION = pixel.Color(10, 0, 0);
-uint32_t COLOUR_TRIP_IN_PROGRESS = pixel.Color(5, 0, 0);
-uint32_t COLOUR_READY = pixel.Color(0, 10, 0);
 
-volatile uint32_t currentPixelColor = COLOUR_OFF;
-volatile uint32_t flashPixelColor = COLOUR_OFF;
-
-/*---------------------------------------------------------------------*/
-
+//--------------------------------------------------------------------------------
 Scheduler runner;
 
-void pirOfflinePeriodCallback();
+#define RUN_ONCE    2
 
+void tFlashLED_ON_Callback();
+void tFlashLED_OFF_Callback();
+Task tFlashLED(50, RUN_ONCE, &tFlashLED_OFF_Callback, &runner, false);
+// for some reason it is triggering ON_Callback first. Weird.
+void tFlashLED_ON_Callback() {
+    pixel.begin();
+    pixel.setPixelColor(0, COLOUR_MOTION);
+    pixel.show();
+    tFlashLED.setCallback(tFlashLED_OFF_Callback);
+}
+void tFlashLED_OFF_Callback() {
+    pixel.begin();
+    pixel.setPixelColor(0, COLOUR_OFF);
+    pixel.show();
+    tFlashLED.setCallback(tFlashLED_ON_Callback);
+}
+
+//--------------------------------------------------------------------------------
 void mqttcallback_Timestamp(byte* payload, unsigned int length) {
 	wifiHelper.mqttPublish(TOPIC_ONLINE, "1", false);
 }
 
-volatile bool checkPirNow = false;
-
-void tServicePirCallback();
-Task tServicePir(500, TASK_FOREVER, &tServicePirCallback, false);
-void tServicePirCallback() {
-	pir.serviceEvents();
-}
-
-/*--------------------------------------------------------------*/
-
-void pir_callback(int eventCode, int eventParams) {
-
-	Serial.println("pir_callback");
-
-	if (eventParams == pir.EV_BUTTON_PRESSED) {
-	}
-
-	if (eventParams == pir.EV_BUTTON_PRESSED) {
-
-		wifiHelper.mqttPublish(TOPIC_MOTION, "1");
-	}
-}
-
-/*--------------------------------------------------------------*/
-
 void mqttcallback_Command(byte *payload, unsigned int length) {
 
-    StaticJsonBuffer<100> jsonBuffer;
-
-    JsonObject& root = jsonBuffer.parseObject(payload);
-
-    if (!root.success()) {
-        Serial.print("parseObject() failed: "); Serial.println((char*) payload);
-        return;
-    }
-
+    JsonObject& root = wifiHelper.mqttGetJson(payload);
     const char* command = root["command"];
     const char* value = root["value"];
 
-    if (strcmp(command, "PIXEL") == 0) {
-        // neopixel version
-        currentPixelColor = getPixelColourFromCsvString(value);
-        pixel.setPixelColor(0, currentPixelColor);
-        pixel.show();
-    }
-    else if (strcmp(command, "FLASH") == 0) {
-        flashPixelColor = getPixelColourFromCsvString(value);
-
-        #define     FLASH_ONCE  1
-        ledManager.flash(pixel, flashPixelColor, 50, FLASH_ONCE);
+    if (strcmp(command, "LED") == 0) {
+    	if (strcmp(value, "FLASH") == 0) {
+    		tFlashLED.restart();
+    	}
     }
 }
-
-uint32_t getPixelColourFromCsvString(const char* val) {
-    const char d[2] = ",";
-    char* colors = strtok((char*)val, d);
-    char* red = colors;
-    colors = strtok(NULL, d);
-    char* grn = colors;
-    colors = strtok(NULL, d);
-    char* blu = colors;
-
-    return pixel.Color(atoi(red), atoi(grn), atoi(blu));
-}
-
 
 /* ----------------------------------------------------------- */
 
 void setup() {
 
-    pixel.begin();
-    pixel.setPixelColor(0, COLOUR_READY);
-    pixel.show();
-
-    delay(50);
-
 	Serial.begin(9600);
 	Serial.println("Booting");
+
+	pixel.begin();
+	pixel.setPixelColor(0, pixel.Color(0,10,0));
+	pixel.show();
 
 	wifiHelper.setupWifi();
 
@@ -187,25 +114,24 @@ void setup() {
 	wifiHelper.mqttAddSubscription(TOPIC_TIMESTAMP, mqttcallback_Timestamp);
     wifiHelper.mqttAddSubscription(TOPIC_COMMAND, mqttcallback_Command);
 
-    pixel.begin();
-    pixel.setPixelColor(0, COLOUR_OFF);
-    pixel.show();
+    delay(100);
+	wifiHelper.loopMqtt();
+	wifiHelper.mqttPublish(TOPIC_EVENT, "Ready");
 
-	//runner.addTask(tFlashMotionLED);
-	runner.addTask(tServicePir);
-
-	tServicePir.restart();
+	pixel.begin();
+	pixel.setPixelColor(0, COLOUR_OFF);
+	pixel.show();
 }
 
 void loop() {
 
-	runner.execute();
-
-    ledManager.service();
-
 	wifiHelper.loopMqtt();
 
 	ArduinoOTA.handle();
+
+    pir.serviceEvents();
+
+    runner.execute();
 
 	delay(10);
 }
